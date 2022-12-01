13418fd3389450f08de36204189830bfe8fe100b
ramenumai <taiki1151001@icloud.com>
2022-12-02 02:35:12 +0900

    init

---
+++ counter/main.js
@@ -0,0 +1,38 @@
+const up = document.querySelector('.up');
+const down = document.querySelector(".down");
+const reset = document.querySelector(".reset");
+const load = document.querySelector(".load");
+
+const count = document.querySelector('.count');
+
+let counter = 0;
+
+//カウントアップ関数
+function countUp() {
+  counter++;
+  count.innerHTML = counter;
+};
+
+//カウントダウン関数
+function countDown() {
+  counter--;
+  count.innerHTML = counter;
+};
+
+//リセット関数
+function countDown() {
+  counter =0;
+  count.innerHTML = counter;
+};
+
+//数えるものをセット
+function set() {
+  let txt = document.querySelector('.txtbox').value;
+  document.querySelector('p').innerHTML = txt;
+  document.querySelector('.txtbox').value = '';
+}
+
+up.addEventListener('click', countUp);
+down.addEventListener('click', countDown);
+reset.addEventListener("click", countDown);
+load.addEventListener("click", set);
